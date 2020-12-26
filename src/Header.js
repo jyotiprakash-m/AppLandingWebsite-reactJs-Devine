051a59585fadd94c59586e898b33657c6e32628a
Jyotiprakash Mohanta <jpmohanta32@gmail.com>
2020-12-26 10:20:27 +0530

    little modification

--- src/Header.js
+++ src/Header.js
@@ -9,7 +9,7 @@ function Header() {
         <div>
             <header class="text-gray-600 body-font bg-blue-200">
             <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row ">
-              <a class="flex title-font font-medium  text-gray-900 mb-4 md:mb-0 items-center text-center">
+              <a href="" class="flex title-font font-medium  text-gray-900 mb-4 md:mb-0 items-center text-center">
                 <img src={logo} alt="logo" className="logoImg" />
                 <span class="ml-3 text-3xl">Devine</span>
               </a>
--- src/Video.js
+++ src/Video.js
@@ -9,7 +9,7 @@ function Video() {
             <div class="container mx-auto flex px-5 py-5 items-center justify-center flex-col">
             <h1 class="text-3xl text-gray-900 mb-5 heading">Video demo</h1>      
                 <div class="lg:w-3/6 md:w-4/6 w-5/6  h-96 mb-10 object-cover object-center rounded" >
-                    <iframe width="100%" height="100%" src="https://www.youtube.com/embed/tgbNymZ7vqY">
+                    <iframe width="100%" height="100%" src="https://www.youtube.com/embed/tgbNymZ7vqY" title="Working of Devine">
                     </iframe>
                 </div>
               
